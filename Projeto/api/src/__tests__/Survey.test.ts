72200e13f630084a5712573d0dc7de93fc7b58e7
9haik1@gmail.com <9haik1@gmail.com>
2021-02-25 21:03:50 -0300

    Adicionando testes automatizados ao projeto

---
+++ Projeto/api/src/__tests__/Survey.test.ts
@@ -0,0 +1,31 @@
+import request from 'supertest';
+import { app } from '../app';
+import createConnection from '../database';
+
+describe("Surveys", () => {
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    it("Should be able to create a new survey", async () => {
+        const response = await request(app).post('/surveys')
+            .send({
+                title: "title example",
+                description: "description exemple"
+            });
+
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty('id');
+    });
+
+    it("should be able get all surveys", async () => {
+        await request(app).post('/surveys')
+            .send({
+                title: "title example",
+                description: "description exemple"
+            });
+        const response = await request(app).get('/surveys');
+        expect(response.body.length).toBe(2);
+    })
+});
--- Projeto/api/src/server.ts
+++ Projeto/api/src/server.ts
@@ -1,11 +1,3 @@
-import 'reflect-metadata';
-import express, { response } from 'express';
-import './database/index';
-import { router } from './routes';
-
-
-const app = express();
-app.use(express.json());
-app.use(router);
+import { app } from './app';
 
 app.listen(3333, () => console.log('Server Online'));
